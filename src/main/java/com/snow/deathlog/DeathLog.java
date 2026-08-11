128a141a204e7afcb0b502856bed62c294928ee5
julzz546-sys <julzz546-sys@users.noreply.github.com>
2026-08-11 17:30:44 +0700

    Initial DeathLog

---
+++ src/main/java/com/snow/deathlog/DeathLog.java
@@ -0,0 +1,59 @@
+package com.snow.deathlog;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+
+public class DeathLog implements ModInitializer {
+
+    public static final String MOD_ID = "deathlog";
+
+    @Override
+    public void onInitialize() {
+        ServerLivingEntityEvents.AFTER_DEATH.register((entity, damageSource) -> {
+            if (!(entity instanceof ServerPlayerEntity player)) {
+                return;
+            }
+
+            var server = player.getServer();
+            if (server == null) {
+                return;
+            }
+
+            String name = player.getName().getString();
+            String uuid = player.getUuidAsString();
+            String dimension = player.getWorld()
+                    .getRegistryKey()
+                    .getValue()
+                    .toString();
+
+            double x = player.getX();
+            double y = player.getY();
+            double z = player.getZ();
+
+            String cause = damageSource.getName();
+
+            server.sendMessage(
+                    Text.literal(
+                            "[DeathLog] " + name
+                                    + " died at "
+                                    + dimension
+                                    + " "
+                                    + String.format("%.1f %.1f %.1f", x, y, z)
+                                    + " | Cause: " + cause
+                    )
+            );
+
+            System.out.println(
+                    "[DeathLog] Player=" + name
+                            + " UUID=" + uuid
+                            + " Dimension=" + dimension
+                            + " X=" + x
+                            + " Y=" + y
+                            + " Z=" + z
+                            + " Cause=" + cause
+            );
+        });
+    }
+}
